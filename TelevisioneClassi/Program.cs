332ff40de003477f3a1a1797cc7b1fa9af58a580
ColombiAlessandro <colombi.alessandro.studente@itispaleocapa.it>
2022-10-04 23:01:09 +0200

    completamento compito
    compito completato

---
+++ TelevisioneClassi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelevisioneClassi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Televisione
+    {
+        public string produttore;
+        public string modello;
+        public string[] connettori;
+        public bool stato;
+        public int canale;
+        public void cambiaStato()
+        {
+            stato = !stato;
+        }
+        public void cambiaCanale()
+        {
+            int tmp= int.Parse(Console.ReadLine());
+            if (tmp >= 0 && tmp <= 999)
+            {
+                canale = int.Parse(Console.ReadLine());
+            }
+            else Console.WriteLine("Canale non trovato");
+        }
+    }
+}
